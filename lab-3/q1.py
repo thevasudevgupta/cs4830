c40dba0a1918c3ebe00c362f95a084af9fb7cd8a
Vasudev Gupta <7vasudevgupta@gmail.com>
2022-02-19 09:37:10 +0000

    q1 works

--- lab-3/q1.py
+++ lab-3/q1.py
@@ -3,25 +3,19 @@ from apache_beam.options.pipeline_options import PipelineOptions
 
 GCS_BUCKET_NAME = "gs://big-data-cs4830"
 TEMP_LOCATION = f"{GCS_BUCKET_NAME}/temp-lab-3"
-OUTPUT_DATA_FILE = f"{GCS_BUCKET_NAME}/lab-3"
+OUTPUT_DATA_FILE = f"{GCS_BUCKET_NAME}/lab-3/q1_output.txt"
 
 INPUT_DATA_FILE = "gs://bdl2022/lines_big.txt"
-# INPUT_DATA_FILE = "lines_big.txt"
 
 beam_options = PipelineOptions(
-    # project="",
+    project="gsoc-wav2vec2",
     job_name="lines-count-lab-3",
     temp_location=TEMP_LOCATION,
-    runner="DirectRunner",
-)
+    runner="DataflowRunner",
+    region="us-central1",
+    )
 
-# with beam.Pipeline(options=beam_options) as p:
-p = beam.Pipeline(options=beam_options)
-print("start")
-lines = p | "Reading from file" >> beam.io.ReadFromText(INPUT_DATA_FILE)
-print("1")
-count = lines | beam.combiners.Count.Globally()
-print("2")
-count | beam.io.WriteToText(OUTPUT_DATA_FILE)
-print("3")
-p.run()
+with beam.Pipeline(options=beam_options) as p:
+    lines = p | "Reading data from file" >> beam.io.ReadFromText(INPUT_DATA_FILE)
+    count = lines | "Counting number of lines in the provided file" >> beam.combiners.Count.Globally()
+    count | "Writing outputs to file" >> beam.io.WriteToText(OUTPUT_DATA_FILE)
